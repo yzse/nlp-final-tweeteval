b01ebe503add04043a4be200e4b7d38d8df5dcb5
yzse <54381977+yzse@users.noreply.github.com>
2023-03-26 20:36:18 +0200

    upload preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,38 +1,32 @@
+#%%
 import requests
 import re
 import pandas as pd
 import numpy as np
+import os
 
+#%%
 # are we working with all tasks?
 TASKS = {
     'emoji': 1, 
     'emotion': 2,
-    'hate': 3,
+    # 'hate': 3,
     'irony': 4,
     'offensive': 5,
     'sentiment': 6,
-    'stance': 7,
-    'abortion': 8,
-    'atheism': 9,
-    'climate': 10,
-    'feminist': 11,
-    'hillary': 12
+    'abortion': 7,
+    'atheism': 8,
+    'climate': 9,
+    'feminist': 10,
+    'hillary': 11
 }
 
-# fetch files
-for data in TASKS.values():
-    task = list(dict.keys())[data-1]
-    print(task)
-    for filetype in ['train', 'val', 'test']:
-        globals()[f'{filetype}_text'] = requests.get(f'https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{task}/{filetype}_text.txt').text
-        globals()[f'{filetype}_label'] = requests.get(f'https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{task}/{filetype}_labels.txt').text
-
+#%%
 def process(label, text):
-    tag = [int(sent) for sent in label.split("\n") if sent.isdigit()]
-    tweet = [text for text in text.split('\n') if text]
-    data = {'tweet': tweet, 'tag': tag}
-    df = pd.DataFrame(data)
-    df['class'] = df.tag.apply(lambda x: 'not-hate' if x == 0 else 'hate')
+    tag = [int(sent) for sent in label.split("\n") if sent.isdigit() and sent.strip()]
+    tweet = [text.strip() for text in text.split('\n') if text.strip()]
+    print(f"Length of tag: {len(tag)}, Length of tweet: {len(tweet)}")
+    df = pd.DataFrame({'tweet': tweet, 'tag': tag})
     return df
 
 # clean up text
@@ -41,14 +35,14 @@ def cleaner(tweet):
     tweet = tweet.lower()
     tweet = re.sub(r'\d+', '', tweet)
     tweet = tweet.replace("user", "")
+    tweet = tweet.replace("semst", "")
+    tweet = ' '.join([w for w in tweet.split() if len(w) > 2])
     return tweet
 
-# applying cleaner function and deleting words with length less than or equal to 2
+# applying cleaner function to dataframe
 def cleanup(df):
     train_cleaned = df['tweet'].apply(cleaner)
-    df['tweet'] = train_cleaned.apply(lambda x: ' '.join([w for w in x.split() if len(w) > 2]))
-
-    return df
+    return train_cleaned
 
 # preparing dataframes for BERT processing
 def cleaned_df():
@@ -62,5 +56,36 @@ def cleaned_df():
 
     lst = [train_cleaned, val_cleaned]
     train_cleaned = pd.concat(lst, ignore_index=True)
+
     return train_cleaned, val_cleaned, test_cleaned
-  
+#%%
+# create directory to save the cleaned dataframes
+if not os.path.exists('cleaned_df'):
+    os.makedirs('cleaned_df')
+#%%
+# fetch files
+for task in enumerate(TASKS, start=1):
+
+    cleaned_dfs = {}
+
+    for filetype in ['train', 'val', 'test']:
+
+        if task[1] in ['abortion', 'atheism', 'climate', 'feminist', 'hillary']:
+            url = f'https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/stance/{task[1]}'
+        else:
+            url = f'https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{task[1]}'
+
+        print(f'{url}/{filetype}_text.txt')
+
+        text = requests.get(f'{url}/{filetype}_text.txt').text
+        label = requests.get(f'{url}/{filetype}_labels.txt').text
+        
+        df = process(label, text)
+        cleaned_df = cleanup(df)
+        cleaned_dfs[filetype] = cleaned_df
+        cleaned_df.to_csv(f'cleaned_df/{task[1]}_{filetype}_cleaned.csv', index=False)
+
+    print(f"{task[1]} dataframes cleaned + saved.")
+
+
+# %%
